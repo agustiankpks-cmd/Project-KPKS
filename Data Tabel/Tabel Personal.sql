3041f8f281238a502090c3d13d767e035f42e65d
agustiankpks-cmd <agustiankpks@gmail.com>
2025-10-22 14:13:51 +0700

    Create Tabel Personal.sql

---
+++ Data Tabel/Tabel Personal.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Personal (
+  Kode varchar(10) NOT NULL,
+  Nama varchar(50) NULL,
+  Tempat varchar(30) NULL,
+  Tgl_Lahir smalldatetime NULL,
+  JK varchar(1) NULL,
+  Unit varchar(10) NULL,
+  Tgl_Masuk smalldatetime NULL,
+  Klasifikasi1 varchar(10) NULL,
+  Klasifikasi2 varchar(10) NULL,
+  Status varchar(10) NULL,
+  Alamat varchar(100) NULL,
+  Identitas varchar(50) NULL,
+  Picture image NULL,
+  Sisa int NULL
+);
