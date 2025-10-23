2d2c06c143543efc390f034d5862f34ce84d88fb
agustiankpks-cmd <agustiankpks@gmail.com>
2025-10-23 11:05:07 +0700

    Update Tabel Personal.sql

--- Data Tabel/Tabel Personal.sql
+++ Data Tabel/Tabel Personal.sql
@@ -1,16 +1,16 @@
 CREATE TABLE Personal (
-  Kode varchar(10) NOT NULL,
-  Nama varchar(50) NULL,
-  Tempat varchar(30) NULL,
-  Tgl_Lahir smalldatetime NULL,
-  JK varchar(1) NULL,
-  Unit varchar(10) NULL,
-  Tgl_Masuk smalldatetime NULL,
-  Klasifikasi1 varchar(10) NULL,
-  Klasifikasi2 varchar(10) NULL,
-  Status varchar(10) NULL,
-  Alamat varchar(100) NULL,
-  Identitas varchar(50) NULL,
+  Kode varchar(10) NOT NULL,--tool yang dipakai txtKode
+  Nama varchar(50) NULL,--tool yang dipakai txtNama
+  Tempat varchar(30) NULL, --tool yang dipakai txtTempat
+  Tgl_Lahir smalldatetime NULL, --dtpLahir
+  JK varchar(1) NULL, --jika laki laki = 1 dan perempuan =2 tool yang dipakai combobox
+  Unit varchar(10) NULL, --combobox --cmbUnit
+  Tgl_Masuk smalldatetime NULL, --dateTimePicker----dtpMasuk
+  Klasifikasi1 varchar(10) NULL,---combobox---cmbKlasifikasi1
+  Klasifikasi2 varchar(10) NULL, ---combobox---cmbKlasifikasi2
+  Status varchar(10) NULL,--berisi aktif dan non aktif --tool combobobox---cmbStatus
+  Alamat varchar(100) NULL, --tool yang dipakai txtAlamat
+  Identitas varchar(50) NULL, ----tool yang dipakai txtIdentitas
   Picture image NULL,
-  NoTelp varchar (12) NULL  
+  NoTelp varchar (12) NULL ----------tool yang dipakai txtNoTelp
 );
